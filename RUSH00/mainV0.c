7ff38f8c8bace5c54e2d950d1c7cd18a1c4fe12e
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2022-09-03 16:08:04 +0200

    C02E01 RUSHOO in progress

--- RUSH00/mainV0.c
+++ RUSH00/mainV0.c
@@ -6,13 +6,11 @@
 /*   By: fcoindre <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 10:33:14 by fcoindre          #+#    #+#             */
-/*   Updated: 2022/09/03 15:06:21 by fcoindre         ###   ########.fr       */
+/*   Updated: 2022/09/03 12:43:25 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
-
 
 void	ft_putchar(char c)
 {
@@ -21,83 +19,20 @@ void	ft_putchar(char c)
 
 void	rush(int x, int y)
 {
-	//char	corner_tlbr = '/';
-	//char	corner_trbl = '\\';
-	//char	star = '*';
+	char	corner_tlbr = '/';
+	char	corner_trbl = '\\';
+	char	star = '*';
 	
-	//int	i;
-	//int j;
+	int	i;
+	int j;
 
-	int nbr_c;
-	int	ind_x;
-	int ind_y;
-	int nbr_c_max;
-
-	nbr_c = 0;
-	ind_x = 0;
-	ind_y = 0;
-	nbr_c_max = x * y;
-
-	while (nbr_c < nbr_c_max)
-	{
-		ind_x = nbr_c % x;
-		ind_y = nbr_c / x;
-		//printf("ind_x = %d ind_y = %d nbr_c = %d\n", ind_x, ind_y, nbr_c);
-		
-		if (nbr_c == 0 || nbr_c == (nbr_c_max-1))
-		{
-			ft_putchar('/');
-		}
-		
-		if (ind_x == (x-1) && ind_y == 0)
-		{
-			ft_putchar('\\');
-			ft_putchar('\n');
-		}
+	x = x - 2;
+	y = y - 2;
 	
-		if (ind_x == 0 && ind_y == (y-1))
-		{
-			ft_putchar('\\');
-		}
-
-		if ((ind_x > 0 && ind_x < (x-1)) && ind_y == 0)
-		{
-			ft_putchar('*');
-		}
-
-		if ((ind_x > 0 && ind_x < (x-1)) && ind_y == (y-1))
-		{
-			ft_putchar('*');
-		}
-
-		if (ind_x == 0 && (ind_y > 0 && ind_y < (y-1)) )
-		{
-			ft_putchar('*');
-		}
-
-		if (ind_x == (x-1) && (ind_y > 0 && ind_y < (y-1)))
-		{
-			ft_putchar('*');
-			ft_putchar('\n');
-		}
-
-		
-		if ((ind_x > 0 && ind_x < (x-1)) && (ind_y > 0 && ind_y < (y-1)) )
-		{
-			ft_putchar(' ');
-		}
-		
-		nbr_c++;
-	}
-
-	
-
-
-	/*	
 	// Premiere ligne :
 	i = 0;
 	ft_putchar (corner_tlbr);
-	while (i < (x-2))
+	while (i < x)
 	{
 		ft_putchar(star);
 		i++;	
@@ -107,11 +42,11 @@ void	rush(int x, int y)
 
 	// Corp du rectangle :
 	j = 0;
-	while (j < (y-2))
+	while (j < y)
 	{
 		i = 0;
 		ft_putchar(star);
-		while (i < (x-2))
+		while (i < x)
 		{
 			ft_putchar(' ');
 			i++;
@@ -124,19 +59,23 @@ void	rush(int x, int y)
 	// Derniere ligne
 	i = 0;
 	ft_putchar (corner_trbl);
-	while (i < (x-2))
+	while (i < x)
 	{
 		ft_putchar(star);
 		i++;	
 	}
 	ft_putchar (corner_tlbr);
 	ft_putchar ('\n');
-	*/
+
 }
 
 int	main(void)
 {
+	int x;
+	int	y;
 
-	rush (5,3);
-	return (0);
+	x = 1;
+	y = 5;
+
+	rush (x,y);
 }
