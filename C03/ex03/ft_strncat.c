f210e083f8fbbfa4045c16340c6dc2a08b0acb63
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2022-09-06 20:00:57 +0200

    C03E04 en cours...

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcoindre <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/06 17:25:14 by fcoindre          #+#    #+#             */
-/*   Updated: 2022/09/06 19:14:58 by fcoindre         ###   ########.fr       */
+/*   Created: 2022/09/06 19:15:55 by fcoindre          #+#    #+#             */
+/*   Updated: 2022/09/06 19:52:49 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
 #include <stdio.h>
 #include <string.h>
 */
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	int	i;
-	int	c;
+	int				i;
+	unsigned int	c;
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -24,7 +24,7 @@ char	*ft_strcat(char *dest, char *src)
 		i++;
 	}
 	c = 0;
-	while (src[c] != '\0')
+	while (c < nb && src[c] != '\0')
 	{
 		dest[i] = src[c];
 		i++;
@@ -34,30 +34,33 @@ char	*ft_strcat(char *dest, char *src)
 	return (dest);
 }
 /*
-int main()
+int main ()
 {
+
 	char s1[15] = "abcd";
 	s1[10] = 'z';
-	char *s2 = "efghij";
+	char *s2 = "12\0 3456789123456";
 
 	char s1_t[15] = "abcd";
 	s1_t[10] = 'z';
-	char *s2_t = "efghij";
+	char *s2_t = "12\0 3456789123456";
+
+	int nb = 9;
 
 	int i = 0;
 	while (i < 15)
 	{
-		printf("i = %d strcat : %c || %c %d\n", i, s1[i], s1_t[i], s1_t[i]);
+		printf("i = %d strncat : %c || %c %d\n", i, s1[i], s1_t[i], s1_t[i]);
 		i++;
 	}
 	printf("-------------------------\n");
-	ft_strcat(s1_t, s2_t);
-	strcat(s1,s2);
+	ft_strncat(s1_t, s2_t, nb);
+	strncat(s1,s2,nb);
 	i = 0;
 	while (i < 15)
 	{
-		printf("i = %d strcat : %c || %c %d \n", i, s1[i], s1_t[i], s1_t[i]);
+		printf("i = %d strncat : %c || %c %d \n", i, s1[i], s1_t[i], s1_t[i]);
 		i++;
 	}
-	return 0;
+
 }*/
