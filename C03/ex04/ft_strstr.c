92e20c46d9a85060a2345f1f3181b6dc41c19712
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2022-09-07 18:47:54 +0200

    C04E03 encours...

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,50 +6,48 @@
 /*   By: fcoindre <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 19:53:13 by fcoindre          #+#    #+#             */
-/*   Updated: 2022/09/07 13:00:01 by fcoindre         ###   ########.fr       */
+/*   Updated: 2022/09/07 16:26:42 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 
-
 char	*ft_strstr(char *str, char *to_find)
 {
+	char	*a;
 	int		i;
 	int		c;
 
-	c = 0;
 	i = 0;
-	if (*to_find == '\0')
+	c = 0;
+	if (to_find[0] == '\0')
 		return (str);
 	while (str[i] != '\0')
 	{
+		if (str[i] == to_find[0])
+			a = &str[i];
 		c = 0;
-		while (str[i] == to_find[c] && to_find[c] != '\0')
+		while (to_find[c] == str[i + c])
 		{
-			i++;
+			if (to_find[c + 1] == '\0')
+				return (a);
 			c++;
 		}
-		if (to_find[c] == '\0')
-			return (str + i - c);
-		else
-			c = 0;
 		i++;
 	}
 	return (NULL);
 }
-
+/*
 int main ()
 {
-	char *str = "pap ppp\0 ppp";
-	char *to_find = "\0 ppp";	
+	char *str = "poiiiiiiiaaam ";
+	char *to_find = "t";
 
 	char *result = ft_strstr(str, to_find);
 	char *result_c = strstr(str, to_find);
 	printf("result       = %s\n", result);
-	printf("result test  = %s\n", result_c);
-
+	printf("result core  = %s\n", result_c);
 
 	return (0);
-}
+}*/
---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcoindre <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 10:59:42 by fcoindre          #+#    #+#             */
+/*   Updated: 2022/09/07 18:39:38 by fcoindre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#define INT_MIN -2147483648
+#define INT_MAX 2147483647
+
+int	ft_find_power(int nb)
+{
+	int	count;
+
+	count = 0;
+	while (nb != 0)
+	{
+		nb = nb / 10;
+		count++;
+	}
+	return (count);
+}
+
+int	ft_find_divider(int count)
+{
+	int	divider;
+
+	divider = 1;
+	while (count > 1)
+	{
+		divider *= 10;
+		count--;
+	}
+	return (divider);
+}
+
+void	print_digit(int a)
+{
+	char	digit_to_print;
+
+	digit_to_print = a + 48;
+	write(1, &digit_to_print, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	int	power;
+	int	divider;
+	int	a;
+
+	power = ft_find_power(nb);
+	divider = ft_find_divider(power);
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else
+	{
+		if (nb < 0)
+		{
+			nb *= (-1);
+			write(1, "-", 1);
+		}
+		while (divider >= 1)
+		{
+			a = nb / divider;
+			nb = nb - (a * divider);
+			print_digit(a);
+			divider /= 10;
+		}
+	}
+}
+/*
+int main(void)
+{
+	ft_putnbr(INT_MIN);
+	return (0);
+}*/
