887c48f2f7c146f997bcda8f927e8c579a436330
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2022-09-08 09:41:43 +0200

    C04E03 en cours

--- C04/ex03/ft_atoi_bck.c
+++ C04/ex03/ft_atoi_bck.c
@@ -6,78 +6,84 @@
 /*   By: fcoindre <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 18:53:00 by fcoindre          #+#    #+#             */
-/*   Updated: 2022/09/08 09:41:04 by fcoindre         ###   ########.fr       */
+/*   Updated: 2022/09/08 08:45:19 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_isspace(char a)
+int ft_isspace(char a)
 {
-	if (a == ' '
+	if (a == ' ' 
 		|| a == '\f'
-		|| a == '\n'
+	 	|| a == '\n'
 	 	|| a == '\r'
 	 	|| a == '\t'
- 		|| a == '\v')
+	 	|| a == '\v')
 	{
 		return (1);
 	}
+
 	return (0);
-}
-
-int	ft_space_min_finder(char *str, int *is_negativ)
-{
-	int	i;
-
-	i = 0;
-	while (ft_isspace(str[i]))
-	{
-		i++;
-	}
-	while (str[i] == '-' || str[i] == '+')
-	{
-		if(str[i] == '-')
-		{
-			*is_negativ *= -1;
-		}
-		i++;
-	}
-	return (i);
 }
 
 int	ft_atoi(char *str)
 {
 	int	i;
+	int ind;
 	int	is_negativ;
+	int	tab[11];
 	int result;
 	int multiplier;
 
 	multiplier = 1;
 	result = 0;
+	ind = 0;
 	is_negativ = 1;
-	i = ft_space_min_finder(str, &is_negativ);
-	while (str[i] >= '0' && str[i] <= '9')
+	i = 0;
+	while (ft_isspace(str[i])) //isspace(3) !!!!!
 	{
 		i++;
 	}
-	i--;
+
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if(str[i] == '-')
+		{
+			is_negativ *= -1;
+		}
+		i++;
+	}
+
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result += (str[i] - 48) * multiplier;
-		multiplier *= 10;
-		i--;
+		tab[ind] = str[i] - 48;
+		i++;
+		ind++;
 	}
+
+	ind--;
+	while (ind >= 0)
+	{
+		result += tab[ind] * multiplier;
+		multiplier *= 10;
+		printf("tab[%d] = %d result = %d\n", ind, tab[ind], result);
+		ind--;
+	}
+
 	return (result * is_negativ);
 }
-/*
+
 int main ()
 {
 	char *input = " \n  ---+--+1234ab567";
+	//input = "a23";
+
+	//printf("%d", isspace("\t"));
 
 	int result = ft_atoi(input);
 
 	printf("result = %d", result);
 
 	return 0;
-}*/
+}
